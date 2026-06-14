322c8630c8a8254e644e91673aa10f12ac58a39d
Devansh-Mishra-30 <devansh30mishra@gmail.com>
2026-06-13 19:39:10 -0500

    Add Day 46 ROS 2 pose publisher node

--- ros2_ws/src/cpp_robotics_sim_ros/src/sim_node.cpp
+++ ros2_ws/src/cpp_robotics_sim_ros/src/sim_node.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <functional>
 
+#include "geometry_msgs/msg/pose2_d.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
@@ -10,16 +11,32 @@ class SimNode : public rclcpp::Node {
 public:
     SimNode()
     : Node("sim_node") {
-        timer_ = create_wall_timer(1000ms, std::bind(&SimNode::timerCallback, this)
-    );
-    RCLCPP_INFO(get_logger(), "Day 45 ROS 2 simulation node started");
+        pose_publisher_ = create_publisher<geometry_msgs::msg::Pose2D>("robot_pose", 10
+        );
+
+        timer_ = create_wall_timer(100ms,
+            std::bind(&SimNode::timerCallback, this)
+
+        );
+
+        RCLCPP_INFO(get_logger(), "Day 46 ROS 2 simulation node started");
     }
 private:
     void timerCallback() {
-        RCLCPP_INFO(get_logger(), "C++ robotics simulator ROS 2 bridge is running");
-    }
+        pose_.x += linear_velocity_ * dt_;
+        pose_.theta = 0.0;
 
+        pose_publisher_->publish(pose_);
+        RCLCPP_INFO(get_logger(), "Published pose: x=%.2f, y=%.2f, theta=%.2f",
+            pose_.x,
+            pose_.y,
+            pose_.theta);
+    }
+    rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr pose_publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
+    geometry_msgs::msg::Pose2D pose_{};
+    double dt_{0.1};
+    double linear_velocity_{ 0.5 };
 };
 
 int main(int argc, char** argv) {
