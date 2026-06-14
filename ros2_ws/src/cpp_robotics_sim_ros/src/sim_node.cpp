3c696f20150d3497eb6bcddf0b961e98411d67e5
Devansh-Mishra-30 <devansh30mishra@gmail.com>
2026-06-14 13:04:07 -0700

    Add Day 47 cmd_vel subscriber

--- ros2_ws/src/cpp_robotics_sim_ros/src/sim_node.cpp
+++ ros2_ws/src/cpp_robotics_sim_ros/src/sim_node.cpp
@@ -1,8 +1,10 @@
 #include <chrono>
 #include <memory>
 #include <functional>
+#include <cmath>
 
 #include "geometry_msgs/msg/pose2_d.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
@@ -14,17 +16,31 @@ public:
         pose_publisher_ = create_publisher<geometry_msgs::msg::Pose2D>("robot_pose", 10
         );
 
+        cmd_subscriber_ = create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, 
+            std::bind(&SimNode::cmdVelCallback, 
+                this, std::placeholders::_1));
+
         timer_ = create_wall_timer(100ms,
             std::bind(&SimNode::timerCallback, this)
 
         );
 
-        RCLCPP_INFO(get_logger(), "Day 46 ROS 2 simulation node started");
+        RCLCPP_INFO(get_logger(), "Day 47 ROS 2 cmd_vel subscriber node started");
     }
 private:
+
+    void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
+        linear_velocity_ = msg->linear.x;
+        angular_velocity_ = msg->angular.z;
+
+        RCLCPP_INFO(get_logger(), "Received cmd_vel: linear.x=%.2f, angular.z=%.2f", linear_velocity_, angular_velocity_);
+    }
+
     void timerCallback() {
-        pose_.x += linear_velocity_ * dt_;
-        pose_.theta = 0.0;
+        pose_.theta += angular_velocity_ * dt_;
+        pose_.theta = std::atan2(std::sin(pose_.theta), std::cos(pose_.theta));
+        pose_.x += linear_velocity_ * std::cos(pose_.theta) * dt_;
+        pose_.y += linear_velocity_ * std::sin(pose_.theta) * dt_;
 
         pose_publisher_->publish(pose_);
         RCLCPP_INFO(get_logger(), "Published pose: x=%.2f, y=%.2f, theta=%.2f",
@@ -33,10 +49,12 @@ private:
             pose_.theta);
     }
     rclcpp::Publisher<geometry_msgs::msg::Pose2D>::SharedPtr pose_publisher_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_subscriber_;
     rclcpp::TimerBase::SharedPtr timer_;
     geometry_msgs::msg::Pose2D pose_{};
     double dt_{0.1};
-    double linear_velocity_{ 0.5 };
+    double linear_velocity_{ 0.0 };
+    double angular_velocity_{ 0.0 };
 };
 
 int main(int argc, char** argv) {
